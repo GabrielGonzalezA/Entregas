9421d9e63dbbd52479170b1e1a1785643d5fc42d
GabrielGonzalezA <gabriel.gonzaz@gmail.com>
2016-03-11 11:15:30 -0300

    Rename 2.1.1 printf_A.c to 2.1 Printf_A.c

--- Aula 2/2.1 Printf_A.c
+++ Aula 2/2.1 Printf_A.c
@@ -3,7 +3,7 @@
 int main()
 {
     printf("Elertronica Embarcada\n");
-    printf("Aula 2 revis„o c\n" "#Adriana #Gabriel #04/03\n");
+    printf("Aula 2 revis√£o c\n" "#Adriana #Gabriel #04/03\n");
     
 
     return 0;
